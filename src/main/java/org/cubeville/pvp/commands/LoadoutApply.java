7a75559e24dfee19c78dd0f3851a62b605cb5747
BuildTools <frediw@users.noreply.github.com>
2018-01-28 23:11:31 +0100

    Switched to 1.12.2, loadout apply command works from console now

--- src/main/java/org/cubeville/pvp/commands/LoadoutApply.java
+++ src/main/java/org/cubeville/pvp/commands/LoadoutApply.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.cubeville.commons.commands.Command;
+
+import org.cubeville.commons.commands.BaseCommand;
 import org.cubeville.commons.commands.CommandExecutionException;
 import org.cubeville.commons.commands.CommandParameterOnlinePlayer;
 import org.cubeville.commons.commands.CommandParameterString;
@@ -14,7 +16,7 @@ import org.cubeville.pvp.CVPvp;
 import org.cubeville.pvp.loadout.LoadoutContainer;
 import org.cubeville.pvp.loadout.LoadoutHandler;
 
-public class LoadoutApply extends Command {
+public class LoadoutApply extends BaseCommand {
 
 	public LoadoutApply() {
 		super("loadout apply");
@@ -25,23 +27,28 @@ public class LoadoutApply extends Command {
 	}
 
 	@Override
-	public CommandResponse execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters)
+	public CommandResponse execute(CommandSender sender, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters)
 			throws CommandExecutionException {
-		String team = "main";
-		Player playerInv = player;
-		LoadoutContainer loadout = CVPvp.getInstance().getLoadoutManager().getLoadoutByName((String) baseParameters.get(0));
-		
-		if (loadout == null)
-			throw new CommandExecutionException("&cLoadout &6" + baseParameters.get(0) + "&c does not exist!");
-		if (parameters.containsKey("team"))
-			team = (String) parameters.get("team");
-		if (parameters.containsKey("player"))
-			playerInv = (Player) parameters.get("player");
-		
-		if (!LoadoutHandler.applyLoadoutToPlayer(playerInv, loadout, team)) {
-			throw new CommandExecutionException("&cSub Loadout &6" + parameters.get("team") + "&c does not exist for &6" + baseParameters.get(0));
-		}
-		return new CommandResponse("&aLoadout &6" + loadout.getInventory(team).getName() + "&a applied to &6" + playerInv.getName());
-	}
 
+            String team = "main";
+            Player playerInv = null;
+            if(sender instanceof Player) playerInv = (Player)sender;
+            
+            LoadoutContainer loadout = CVPvp.getInstance().getLoadoutManager().getLoadoutByName((String) baseParameters.get(0));
+
+            if (loadout == null)
+                throw new CommandExecutionException("&cLoadout &6" + baseParameters.get(0) + "&c does not exist!");
+            if (parameters.containsKey("team"))
+                team = (String) parameters.get("team");
+            if (parameters.containsKey("player"))
+                playerInv = (Player) parameters.get("player");
+
+            if(playerInv == null)
+                throw new CommandExecutionException("&cNeed player argument if run from console.");
+            
+            if (!LoadoutHandler.applyLoadoutToPlayer(playerInv, loadout, team))
+                throw new CommandExecutionException("&cSub Loadout &6" + parameters.get("team") + "&c does not exist for &6" + baseParameters.get(0));
+            
+            return new CommandResponse("&aLoadout &6" + loadout.getInventory(team).getName() + "&a applied to &6" + playerInv.getName());
+	}
 }
